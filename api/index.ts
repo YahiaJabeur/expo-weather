57800a7e375ecd6136553a53b768b94943a41262
YahiaJabeur <yahiajabeur@gmail.com>
2024-06-03 23:38:26 +0200

    feat: create api http request

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,29 @@
 import { API_KEY, BASE_URL } from "@/constants/config";
+import { City } from "@/types/City";
+import { CurrentWeather, Forecast } from "@/types/CurrentWeather";
 import axios from "axios";
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
-export const getLocation = async (location: string) => {
+export const getLocation = async (location: string): Promise<City[]> => {
   const { data } = await api.get(`/search.json?key=${API_KEY}&q=${location}`);
   return data;
 };
 
-export const getCurrentWeather = async (locationUrl: string) => {
+export const getCurrentWeather = async (
+  locationUrl: string,
+): Promise<CurrentWeather> => {
   const { data } = await api.get(
-    `/current.json?key=${API_KEY}&q=${location}&aqi=no`,
+    `/current.json?key=${API_KEY}&q=${locationUrl}&aqi=no`,
+  );
+  return data;
+};
+
+export const getForecast = async (locationUrl: string): Promise<Forecast> => {
+  const { data } = await api.get(
+    `/forecast.json?key=${API_KEY}&q=${locationUrl}&days=2&aqi=no&alerts=no`,
   );
   return data;
 };
---
+++ types/CurrentWeather.ts
@@ -0,0 +1,101 @@
+interface Location {
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  tz_id: string;
+  localtime_epoch: number;
+  localtime: string;
+}
+
+interface Condition {
+  text: string;
+  icon: string;
+  code: number;
+}
+
+interface Current {
+  last_updated_epoch: number;
+  last_updated: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: Condition;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  windchill_c: number;
+  windchill_f: number;
+  heatindex_c: number;
+  heatindex_f: number;
+  dewpoint_c: number;
+  dewpoint_f: number;
+  vis_km: number;
+  vis_miles: number;
+  uv: number;
+  gust_mph: number;
+  gust_kph: number;
+}
+
+export interface Measurements {
+  time_epoch: number;
+  time: string;
+  temp_c: number;
+  temp_f: number;
+  is_day: number;
+  condition: Condition;
+  wind_mph: number;
+  wind_kph: number;
+  wind_degree: number;
+  wind_dir: string;
+  pressure_mb: number;
+  pressure_in: number;
+  precip_mm: number;
+  precip_in: number;
+  snow_cm: number;
+  humidity: number;
+  cloud: number;
+  feelslike_c: number;
+  feelslike_f: number;
+  windchill_c: number;
+  windchill_f: number;
+  heatindex_c: number;
+  heatindex_f: number;
+  dewpoint_c: number;
+  dewpoint_f: number;
+  will_it_rain: number;
+  chance_of_rain: number;
+  will_it_snow: number;
+  chance_of_snow: number;
+  vis_km: number;
+  vis_miles: number;
+  gust_mph: number;
+  gust_kph: number;
+  uv: number;
+}
+
+export interface CurrentWeather {
+  location: Location;
+  current: Current;
+}
+
+interface Forecastday {
+  date: string;
+  hour: Measurements[];
+}
+
+export interface Forecast {
+  current: Current;
+  forecast: { forecastday: Forecastday[] };
+  location: Location;
+}
