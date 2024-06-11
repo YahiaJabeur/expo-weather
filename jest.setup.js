db0ab8b3ab2fbab46bc9af622e99bb73cb997b53
YahiaJabeur <yahiajabeur@gmail.com>
2024-06-11 22:24:50 +0200

    chore: remove async-storage dependency

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,3 @@ import "./unistyles";
 
 jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
 jest.mock("@testing-library/jest-native/extend-expect");
-jest.mock("@react-native-async-storage/async-storage", () =>
-  require("@react-native-async-storage/async-storage/jest/async-storage-mock"),
-);
