c706a252e855d8504f97fe5a51cf84ef7ba44a95
YahiaJabeur <yahiajabeur@gmail.com>
2024-06-19 22:00:58 +0200

    refactor: simplify DevButton code

--- src/components/DevButton.tsx
+++ src/components/DevButton.tsx
@@ -1,15 +1,10 @@
+import { clearStorage } from "@/libs/localStorage";
 import { Button } from "react-native";
 
-import { clearStorage } from "../libs/localStorage";
-
 export const DevButton = () => {
-  const clearAndRefresh = async () => {
-    await clearStorage();
-    window.location.reload();
-  };
   return (
     <Button
-      onPress={clearAndRefresh}
+      onPress={clearStorage}
       title="Dev button - Clear storage and refresh Application"
     />
   );
