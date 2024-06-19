f5b2f722e844ddc0c3356acd95a4de6251f584ff
YahiaJabeur <yahiajabeur@gmail.com>
2024-06-19 22:19:42 +0200

    refactor: useState instead useeffect in landing screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,9 @@
 import { getStoredData } from "@/libs/localStorage";
 import { Redirect, Stack } from "expo-router";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  const [location, setLocation] = useState<string | undefined>(undefined);
-
-  // use memo instead
-  useEffect(() => {
-    const storedCity = getStoredData("SELECTED_LOCATION_KEY");
-    setLocation(storedCity);
-  }, [location]);
+  const [location] = useState(getStoredData("SELECTED_LOCATION_KEY"));
 
   return (
     <>
