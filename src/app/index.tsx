6c05fe68c696ed19d278f1f81c3b7fb0b686cd98
YahiaJabeur <yahiajabeur@gmail.com>
2024-06-23 22:31:49 +0200

    feat: add ActivityIndicator to landing screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,23 +1,35 @@
 import { Redirect, Stack } from "expo-router";
 import React, { useState } from "react";
+import { ActivityIndicator, View } from "react-native";
+import { createStyleSheet, useStyles } from "react-native-unistyles";
 
 import { getStoredData } from "@/libs/localStorage";
 
 export default function App() {
+  const { styles } = useStyles(stylesheet);
   const [location] = useState(getStoredData("SELECTED_LOCATION_KEY"));
 
   return (
-    <>
+    <View style={styles.container}>
       <Stack.Screen
         options={{
           headerShown: false,
         }}
       />
+      <ActivityIndicator size={"large"} />
       {location ? (
         <Redirect href={"home"} />
       ) : (
         <Redirect href={"addLocation"} />
       )}
-    </>
+    </View>
   );
 }
+
+const stylesheet = createStyleSheet((theme) => ({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: theme.colors.background,
+  },
+}));
