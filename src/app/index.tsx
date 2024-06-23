6f7a13e685ed669680b9ef3afdf6710a0aff5ebb
YahiaJabeur <yahiajabeur@gmail.com>
2024-06-23 22:48:38 +0200

    refactor: remove locale state from landing screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Stack } from "expo-router";
-import React, { useState } from "react";
+import React from "react";
 import { ActivityIndicator, View } from "react-native";
 import { createStyleSheet, useStyles } from "react-native-unistyles";
 
@@ -7,7 +7,7 @@ import { getStoredData } from "@/libs/localStorage";
 
 export default function App() {
   const { styles } = useStyles(stylesheet);
-  const [location] = useState(getStoredData("SELECTED_LOCATION_KEY"));
+  const location = getStoredData("SELECTED_LOCATION_KEY");
 
   return (
     <View style={styles.container}>
