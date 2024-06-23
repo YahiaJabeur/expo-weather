6ff7ae6bbd3714371d56973492686993ebe818d3
YahiaJabeur <yahiajabeur@gmail.com>
2024-06-24 00:13:22 +0200

    feat: create resetApp func in dev button

--- src/components/DevButton.tsx
+++ src/components/DevButton.tsx
@@ -1,11 +1,17 @@
+import * as Updates from "expo-updates";
 import { Button } from "react-native";
 
 import { clearStorage } from "@/libs/localStorage";
 
 export const DevButton = () => {
+  const resetApp = () => {
+    clearStorage();
+    Updates.reloadAsync();
+  };
+
   return (
     <Button
-      onPress={clearStorage}
+      onPress={resetApp}
       title="Dev button - Clear storage and refresh Application"
     />
   );
