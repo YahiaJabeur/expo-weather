79b2f7e88e21ecdd7802c240702a2fd88c774521
YahiaJabeur <yahiajabeur@gmail.com>
2024-06-28 23:15:24 +0200

    feat: handle http errors

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,36 @@
 import "react-native-reanimated";
 import "../unistyles";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { Stack } from "expo-router";
+import Toast from "react-native-toast-message";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (error) =>
+      Toast.show({
+        type: "error",
+        text1: "Something is wrong!",
+        text2: error.message,
+        position: "bottom",
+      }),
+  }),
+});
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
       <Stack screenOptions={{ headerBackTitleVisible: false }} />
+      <Toast />
     </QueryClientProvider>
   );
 }
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -42,7 +42,6 @@ export default function Home() {
 
   const memoizedMeasurements = useMemo(() => {
     if (data) {
-      // TODO
       const measurement = [
         ...data.forecast.forecastday[0].hour,
         ...data.forecast.forecastday[1].hour,
